a33773f9ce3d1a86c8ae5b649e82e7e220ea81a6
Syd Bailey <sydabailey@gmail.com>
2019-07-11 00:28:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ function logWhisper(string) {
 console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-    if (string.toLowerCase()===string)
+if ('I love you, Grandma'===string)
+return 'I love you, too.'
+if (string.toLowerCase()===string)
 return "I can't hear you!"
 if (string.toUpperCase()===string)
 return 'YES INDEED!'
-if ('I love you, Grandma'===string)
-return 'I love you, too.'
+
 }
